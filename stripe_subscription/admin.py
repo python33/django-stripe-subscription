93b552aede355d326674979353f78b37ec835a66
Petr Dovnar <petrdovnar@gmail.com>
2021-03-19 06:27:44 +0300

    Plan admin update: publeshed added to list display and filter.

--- stripe_subscription/admin.py
+++ stripe_subscription/admin.py
@@ -5,7 +5,8 @@ from . import models
 
 @admin.register(models.Plan)
 class PlanAdmin(admin.ModelAdmin):
-    list_display = ('title', 'price_id', 'product_id', 'price')
+    list_display = ('title', 'price_id', 'product_id', 'price', 'published')
+    list_filter = ('published',)
 
     def get_readonly_fields(self, request, obj=None):
         readonly_fields = ['price_id', 'product_id']
